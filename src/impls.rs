cdc64d704a1b35a71260141f5067be0f15db15fa
Bartłomiej Kamiński <fizyk20@gmail.com>
2017-04-08 18:42:04 +0200

    implement AsRef, AsMut, Borrow, BorrowMut, Hash for GenericArray

--- src/impls.rs
+++ src/impls.rs
@@ -1,9 +1,9 @@
 use super::{ArrayLength, GenericArray};
+use core::{mem, ptr};
+use core::borrow::{Borrow, BorrowMut};
 use core::cmp::Ordering;
-use core::fmt;
-use core::fmt::Debug;
-use core::mem;
-use core::ptr;
+use core::fmt::{self, Debug};
+use core::hash::{Hash, Hasher};
 use nodrop::NoDrop;
 
 impl<T: Default, N> Default for GenericArray<T, N>
@@ -69,5 +69,47 @@ impl<T: Debug, N> Debug for GenericArray<T, N>
 {
     fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
         self[..].fmt(fmt)
+    }
+}
+
+impl<T, N> Borrow<[T]> for GenericArray<T, N>
+    where N: ArrayLength<T>
+{
+    fn borrow(&self) -> &[T] {
+        &self[..]
+    }
+}
+
+impl<T, N> BorrowMut<[T]> for GenericArray<T, N>
+    where N: ArrayLength<T>
+{
+    fn borrow_mut(&mut self) -> &mut [T] {
+        &mut self[..]
+    }
+}
+
+impl<T, N> AsRef<[T]> for GenericArray<T, N>
+    where N: ArrayLength<T>
+{
+    fn as_ref(&self) -> &[T] {
+        &self[..]
+    }
+}
+
+impl<T, N> AsMut<[T]> for GenericArray<T, N>
+    where N: ArrayLength<T>
+{
+    fn as_mut(&mut self) -> &mut [T] {
+        &mut self[..]
+    }
+}
+
+impl<T: Hash, N> Hash for GenericArray<T, N>
+    where N: ArrayLength<T>
+{
+    fn hash<H>(&self, state: &mut H)
+        where H: Hasher
+    {
+        Hash::hash(&self[..], state)
     }
 }
