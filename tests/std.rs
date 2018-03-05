be4c86e6f479f821215c2b55fc9026d1916fad81
novacrazy <novacrazy@gmail.com>
2018-03-04 22:58:01 -0600

    Tweak tests for references

--- tests/std.rs
+++ tests/std.rs
@@ -12,10 +12,10 @@ use generic_array::functional::*;
 
 pub fn test_generic<S>(s: S)
     where
-        S: FunctionalSequence<i32>,
-        SequenceItem<S>: Add<i32, Output=i32>,
-        S: MappedGenericSequence<i32, i32>,
-        MappedSequence<S, i32, i32>: Debug
+        S: FunctionalSequence<i32>,            // `.map`
+        SequenceItem<S>: Add<i32, Output=i32>, // `+`
+        S: MappedGenericSequence<i32, i32>,    // `i32` -> `i32`
+        MappedSequence<S, i32, i32>: Debug     // println!
 {
     let a = s.map(|x| x + 1);
 
