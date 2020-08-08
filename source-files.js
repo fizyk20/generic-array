63e4a9bdefd4db2eb046902833ed96222b52fc6d
Travis CI User <travis@example.org>
2020-08-08 13:50:53 +0000

    Update documentation

---
+++ source-files.js
@@ -0,0 +1,5 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["generic_array"] = {"name":"","files":["arr.rs","functional.rs","hex.rs","impl_serde.rs","impls.rs","iter.rs","lib.rs","sequence.rs"]};
+sourcesIndex["serde"] = {"name":"","dirs":[{"name":"de","files":["from_primitive.rs","ignored_any.rs","impls.rs","mod.rs","utf8.rs","value.rs"]},{"name":"private","files":["de.rs","macros.rs","mod.rs","ser.rs"]},{"name":"ser","files":["fmt.rs","impls.rs","impossible.rs","mod.rs"]}],"files":["export.rs","integer128.rs","lib.rs","macros.rs"]};
+sourcesIndex["typenum"] = {"name":"","files":["array.rs","bit.rs","int.rs","lib.rs","marker_traits.rs","operator_aliases.rs","private.rs","type_operators.rs","uint.rs"]};
+createSourceSidebar();
