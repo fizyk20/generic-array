8288213cdf3e01a25b65404dfb88700519185698
Alan Egerton <eggyal@gmail.com>
2022-09-10 15:44:11 +0100

    constify arr macro
    Closes #110

--- tests/arr.rs
+++ tests/arr.rs
@@ -25,3 +25,9 @@ fn with_trailing_comma() {
     let ar = arr![u8; 10, 20, 30, ];
     assert_eq!(format!("{:x}", ar), "0a141e");
 }
+
+#[test]
+fn const_context() {
+    const AR: generic_array::GenericArray<u8, typenum::U3> = arr![u8; 10, 20, 30];
+    assert_eq!(format!("{:x}", AR), "0a141e");
+}
