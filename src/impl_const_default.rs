3e00487fe00ba74c1e510f8b378fd213b4e9e5fa
Alan Egerton <eggyal@gmail.com>
2022-09-11 11:22:51 +0100

    Trait for default const values
    Utilises the `const-default` crate (behind a feature gate) to introduce
    a trait defining an associated constant of the `Self` type.  An
    implementation is provided for `GenericArray`, providing means of
    constructing default `GenericArray`s in const context.
    
    This plugs a gap that exists even after #130 was merged, as generic type
    parameters cannot currently be referenced in const context (e.g. if the
    repetition length provided to the `arr!` macro comes from a type
    parameter).

---
+++ src/impl_const_default.rs
@@ -0,0 +1,27 @@
+//! Const default implementation
+
+use crate::{ArrayLength, GenericArray, GenericArrayImplEven, GenericArrayImplOdd};
+use const_default::ConstDefault;
+use core::marker::PhantomData;
+
+impl<T, U: ConstDefault> ConstDefault for GenericArrayImplEven<T, U> {
+    const DEFAULT: Self = Self {
+        parent1: U::DEFAULT,
+        parent2: U::DEFAULT,
+        _marker: PhantomData,
+    };
+}
+
+impl<T: ConstDefault, U: ConstDefault> ConstDefault for GenericArrayImplOdd<T, U> {
+    const DEFAULT: Self = Self {
+        parent1: U::DEFAULT,
+        parent2: U::DEFAULT,
+        data: T::DEFAULT,
+    };
+}
+
+impl<T, U: ArrayLength<T>> ConstDefault for GenericArray<T, U>
+where U::ArrayType: ConstDefault,
+{
+    const DEFAULT: Self = Self { data: ConstDefault::DEFAULT };
+}
